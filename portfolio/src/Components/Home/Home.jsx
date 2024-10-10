ac0e562b001944653a7ee7cd4e05db83b554121c
ss-karthik <maniac.sskarthik@gmail.com>
2024-10-11 00:37:53 +0530

    Add home page and social links to Navbar

--- portfolio/src/Components/Home/Home.jsx
+++ portfolio/src/Components/Home/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import photo from '../../assets/photo.jpg'
 const Home = () => {
   return (
-    <div className='pl-16'>
-      Home
+    <div className='md:pl-16 pl-14 bg-emerald-900 min-h-screen h-full w-full flex flex-col items-center py-10 gap-5'>
+    <img src={photo} className='rounded-full max-w-64'/>
+      <h1 className='text-6xl text-white'>Karthik</h1>
+      <h1 className='text-xl text-white text-center '> 
+        Hey there fellow humans! I am Karthik. 
+        <br/><br/>
+        I am a CompSci student and I have a great fascination for tech. I also make videos on my YouTube channel.
+        I am a noob (if I say so myself) in the software development space. This is one of my attempts to put myself and my code out in the open and learn in public. 
+        <br/><br/>
+        This is my personal space where I display to you at a glance my projects, skills and the best parts of myself to you.
+        <br/><br/>
+        Feel free to reach me by dropping an e-mail at maniac.sskarthik@gmail.com or drop a DM on my Instagram.
+        <br/><br/>
+        oh btw, I use linux.
+    </h1>
     </div>
   )
 }
--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { HomeIcon, GalleryVerticalEnd, ScrollText } from 'lucide-react'
+import { HomeIcon, GalleryVerticalEnd, ScrollText, Instagram, Github, Youtube } from 'lucide-react';
+
 const Navbar = () => {
   return (
-    <div className='fixed top-0 left-0 h-full w-14 z-50 bg-amber-600 flex flex-col items-center py-5 gap-5'>
-        <Link to="/"><HomeIcon size={40}/></Link>
-        <Link to="/projects"><GalleryVerticalEnd size={40}/></Link>
-        <Link to="/skills"><ScrollText size={40}/></Link>
+    <div className='fixed top-0 left-0 h-full md:w-14 w-12 z-50 bg-gradient-to-b from-cyan-300 to-indigo-300 flex flex-col items-center justify-between py-5 gap-5'>
+        <div className='flex flex-col items-center gap-5'>
+            <Link to="/"><HomeIcon size={40}/></Link>
+            <Link to="/projects"><GalleryVerticalEnd size={40}/></Link>
+            <Link to="/skills"><ScrollText size={40}/></Link>
+        </div>
+        <div className='flex flex-col items-center gap-5'>
+            <a className='cursor-pointer' href='https://github.com/ss-karthik' target="_blank" rel="noopener noreferrer"><Github size={40}/></a>
+            <a className='cursor-pointer' href='https://www.youtube.com/@Maniakss' target="_blank" rel="noopener noreferrer"><Youtube size={40}/></a>        
+            <a className='cursor-pointer' href='https://www.instagram.com/karthiks_ig/' target="_blank" rel="noopener noreferrer"><Instagram size={40}/></a>
+        </div>
     </div>
   )
 }
