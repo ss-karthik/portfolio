5095f447512e91cd3547279dece7eb2a1436a111
ss-karthik <maniac.sskarthik@gmail.com>
2024-10-31 22:14:28 +0530

    Add navlink to blog

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { HomeIcon, GalleryVerticalEnd, ScrollText, Instagram, Github, Youtube } from 'lucide-react';
+import { HomeIcon, GalleryVerticalEnd, ScrollText, Instagram, Github, Youtube, Sparkle } from 'lucide-react';
 
 const Navbar = () => {
   return (
@@ -8,9 +8,10 @@ const Navbar = () => {
         <div className='flex flex-col items-center gap-5'>
             <Link to="/"><HomeIcon size={40}/></Link>
             <Link to="/projects"><GalleryVerticalEnd size={40}/></Link>
-            <Link to="/skills"><ScrollText size={40}/></Link>
+            <Link to="/skills"><Sparkle size={40}/></Link>
         </div>
         <div className='flex flex-col items-center gap-5'>
+        <a className='cursor-pointer' href='https://karthikss.netlify.app/' target="_blank" rel="noopener noreferrer"><ScrollText size={40}/></a>
             <a className='cursor-pointer' href='https://github.com/ss-karthik' target="_blank" rel="noopener noreferrer"><Github size={40}/></a>
             <a className='cursor-pointer' href='https://www.youtube.com/@Maniakss' target="_blank" rel="noopener noreferrer"><Youtube size={40}/></a>        
             <a className='cursor-pointer' href='https://www.instagram.com/karthiks_ig/' target="_blank" rel="noopener noreferrer"><Instagram size={40}/></a>
