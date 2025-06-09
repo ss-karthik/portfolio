e2d8739c425dd9785c1241b6d315db9d149ce17f
ss-karthik <maniac.sskarthik@gmail.com>
2025-06-09 15:05:34 +0530

    Add 2 new project cards - Pratibimb and Habitopia(team)

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -3,8 +3,8 @@ import {Github} from 'lucide-react'
 import {Link} from 'lucide-react'
 const ProjectCard = ({projectName, deployURL, githubURL, logo, projectDesc}) => {
   return (
-    <div className='rounded-lg p-2 w-72 h-96 flex flex-col gap-3 justify-between items-center border border-sky-600 bg-teal-50 shadow-md hover:bg-teal-100 transition-colors duration-300'>
-      <img src={logo} className='rounded-xl max-w-56 '></img>
+    <div className='rounded-lg p-2 w-72 h-108 flex flex-col gap-3 justify-between items-center border border-sky-600 bg-teal-50 shadow-md hover:bg-teal-100 transition-colors duration-300'>
+      <img src={logo} className='rounded-xl w-56 h-56 object-cover'></img>
       <div className='flex-grow'>
       <h1 className='text-xl text-center'>{projectName}</h1>
       <h1 className='text-md text-center'>{projectDesc}</h1>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -9,7 +9,8 @@ import f1timerLogo from '../../assets/projects/f1timer.png'
 import calkulateLogo from '../../assets/projects/calkulate.png'
 import drummerLogo from '../../assets/projects/drummer.png'
 import tempchatLogo from '../../assets/projects/tempchat.png'
-
+import habitopiaLogo from '../../assets/projects/Habitopia.jpeg'
+import pratibimbLogo from '../../assets/projects/Pratibimb.png'
 
 
 
@@ -18,16 +19,30 @@ const Projects = () => {
     <div className='md:pl-16 pl-14 bg-emerald-900 min-h-screen h-full w-full flex flex-col items-center py-10 gap-5'>
       <h1 className='text-6xl text-white'>Projects</h1>
       <div className='md:m-10 flex justify-center items-center flex-wrap gap-5'>
+        <ProjectCard 
+        projectName={"Pratibimb"}
+        projectDesc={"A Health Tracking system based on BMI measurements from an IOT Device + AI medical report analysis"}
+        deployURL={"https://www.mypratibimb.in/"}
+        githubURL={"https://github.com/ss-karthik/pratibimb"}
+        logo={pratibimbLogo}
+      />
+        <ProjectCard 
+        projectName={"Habitopia (Team Project)"}
+        projectDesc={"A Habit-Tracking webapp that uses streaks to motivate. Built the data visualizations."}
+        deployURL={"https://habitopia-sdg.vercel.app/"}
+        githubURL={"https://github.com/Sathya-reddy1658/Habitopia/commits?author=ss-karthik"}
+        logo={habitopiaLogo}
+      />
       <ProjectCard 
         projectName={"Tempchat"}
-        projectDesc={"A Websocket based Temporary Chatroom"}
+        projectDesc={"A Websocket based Temporary Chatroom with chat export and room generation"}
         deployURL={"https://tempchat-one.vercel.app/"}
         githubURL={"https://github.com/ss-karthik/tempchat"}
         logo={tempchatLogo}
       />
       <ProjectCard 
         projectName={"Typetester"}
-        projectDesc={"A simple typing speed tester - 20 words, 5 letters each"}
+        projectDesc={"A simple typing speed tester - 20 words, 5 letters each. Built in react in a boring class"}
         deployURL={"https://typetester-mocha.vercel.app/"}
         githubURL={"https://github.com/ss-karthik/typetester"}
         logo={typtesterLogo}
@@ -48,7 +63,7 @@ const Projects = () => {
       />
       <ProjectCard 
         projectName={"Freedvice"}
-        projectDesc={"A Free Advice generator"}
+        projectDesc={"A Free Advice generator. The cat can be a bit scary and direct. Sorry."}
         deployURL={"https://freedvice.onrender.com/"}
         githubURL={"https://github.com/ss-karthik/freedvice"}
         logo={freedviceLogo}
@@ -69,14 +84,14 @@ const Projects = () => {
       />
       <ProjectCard 
         projectName={"Calkulate"}
-        projectDesc={"A Simple Calculator app."}
+        projectDesc={"A Simple Calculator app. Try dividing by 0."}
         deployURL={"https://ss-karthik.github.io/calculator/"}
         githubURL={"https://github.com/ss-karthik/calculator"}
         logo={calkulateLogo}
       />
       <ProjectCard 
         projectName={"Drum Machine"}
-        projectDesc={"A Drum Machine."}
+        projectDesc={"A Drum Machine, click and drum go bang bang. Self explanatory"}
         deployURL={"https://drummer-ssk.netlify.app/"}
         githubURL={"https://github.com/ss-karthik/drum-machine"}
         logo={drummerLogo}
